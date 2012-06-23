c19792b722e6f41cdf9d12aad47c728b1cdea5a8
niketanpansare@gmail.com <niketanpansare@gmail.com@1b9054cd-1b79-2cf7-c432-7e3b66cff6bb>
2012-06-23 21:09:29 +0000

    Updated installation file

--- trunk/src/install_yadmt.sh
+++ trunk/src/install_yadmt.sh
@@ -9,15 +9,15 @@ fi
 sudo apt-get update
 sudo apt-get install build-essential
 sudo apt-get install unzip
+sudo apt-get update
 
 USER_HOME=$(eval echo ~${SUDO_USER})
 YADMT_DIR=$USER_HOME"/yadmt"
-INSTALL_DIR=`pwd`
 
 echo "Installing yadmt files"
 mkdir $YADMT_DIR 
 chmod +x *.sh
-cp yadmt.sh $YADMT_DIR"/yadmt"
+cp yadmt.sh $YADMT_DIR
 cp getParameterName.sh $YADMT_DIR"/getParameterName"
 cp runClassifier.sh $YADMT_DIR"/runClassifier" 
 g++ GenerateArffFiles.cpp Assert.cpp -o $YADMT_DIR"/GenerateArffFiles"
@@ -58,7 +58,7 @@ wget "http://prdownloads.sourceforge.net/weka/weka-3-6-7.zip"
 unzip "weka-3-6-7.zip"
 cp "weka-3-6-7/weka.jar" $YADMT_DIR
 cd $YADMT_DIR
-rm -rf $YADMT_DIR"/weka-3-6-7/"
+rm -rf $YADMT_DIR"/weka/"
 
 
 if ! parallel --help &> /dev/null
@@ -67,6 +67,7 @@ then
   cd $YADMT_DIR  
   wget "http://ftp.gnu.org/gnu/parallel/parallel-20120522.tar.bz2"
   tar -xjf "parallel-20120522.tar.bz2"
+  rm "parallel-20120522.tar.bz2"
   mv parallel-20120522 gnu-parallel
   cd gnu-parallel 
   ./configure
