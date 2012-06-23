5f445ca789c3ebbca4e7727747fd19c80ad79d45
niketanpansare@gmail.com <niketanpansare@gmail.com@1b9054cd-1b79-2cf7-c432-7e3b66cff6bb>
2012-06-23 17:18:28 +0000

    Add scp code. Not tested yet. However, code for single machine is working with exception of naive bayes

--- src/runClassifier.sh
+++ src/runClassifier.sh
@@ -11,6 +11,9 @@
 # Has to be of format "configFile,inputFile,cycleNum,classifier,optional_params" without any spaces
 INPUT_CONFIGURATION=$1
 PGM_NAME=$0
+USER_HOME=$(eval echo ~${SUDO_USER})
+YADMT_DIR=$USER_HOME"/yadmt"
+LOCKDIR="/tmp/yadmt.lock"
 
 # set CONFIG_FILE, INPUT_FILE and CYCLE_NUM
 Array=(`echo $INPUT_CONFIGURATION | tr "," "\n"`)
@@ -34,59 +37,8 @@ elif [ "$CLASSIFIER" == "svm_rbf" ]; then
   fi
 fi
 
-NUM_LINES=`cat $INPUT_FILE | wc -l`
-NUM_TRAIN=$(($NUM_LINES*80/100)) 
-NUM_TEST=$(($NUM_LINES*20/100))
-
-# Sanity check
-TOTAL_ITEMS=$((NUM_TRAIN+NUM_TEST))
-if [ "$TOTAL_ITEMS" -gt "$NUM_LINES" ]; then
-  echo >&2 $PGM_NAME " - Incorrect number for training and test data"
-  exit 1
-fi
-
-#Read variables: MAX_MEMORY_CLASSIFIER, ACCURACY_FILE, RESULTS_FILE
-source "./"$CONFIG_FILE
-
-if [ -z "${MAX_MEMORY_CLASSIFIER}" ]; then
-  MAX_MEMORY_CLASSIFIER=1000
-fi
-
-###########################
-# Generate train and test files
-RANDOMIZED_INPUT_FILE="input-"$RANDOM"-"$RANDOM"-"$RANDOM".txt"
-if ! sort -R $INPUT_FILE > $RANDOMIZED_INPUT_FILE 2> /dev/null
-then
-  echo >&2 $PGM_NAME " - Your system doesnot support \"sort -R\" option required to randomize the input file."
-  exit 1
-fi
-
-TRAIN_FILE=$RANDOMIZED_INPUT_FILE".train"
-TEST_FILE=$RANDOMIZED_INPUT_FILE".test"
-cat $RANDOMIZED_INPUT_FILE | head -n $NUM_TRAIN > $TRAIN_FILE
-cat $RANDOMIZED_INPUT_FILE | tail -n $NUM_TEST > $TEST_FILE
-###########################
-
-if ! NUM_FEATURES=`./GetMetaInformationOfInputSVM $RANDOMIZED_INPUT_FILE numFeatures` 2> /dev/null
-then
-  echo >&2 $PGM_NAME " - Incorrect classes in input file:" $INPUT_FILE  "Make sure you assign values for all classes (eg: you cannot have classes 1,2,3,5). If there are just 2 classes, assign values -1 and 1."
-  rm $RANDOMIZED_INPUT_FILE $TRAIN_FILE $TEST_FILE &> /dev/null
-  exit 1
-fi
-if ! NUM_CLASSES=`./GetMetaInformationOfInputSVM $RANDOMIZED_INPUT_FILE numClasses` 2> /dev/null
-then
-  echo >&2 $PGM_NAME " - Incorrect classes in input file1:" $INPUT_FILE  "Make sure you assign values for all classes (eg: you cannot have classes 1,2,3,5). If there are just 2 classes, assign values -1 and 1."
-  rm $RANDOMIZED_INPUT_FILE $TRAIN_FILE $TEST_FILE &> /dev/null
-  exit 1
-fi
-PARAM_NAME=`./getParameterName $RANDOMIZED_INPUT_FILE`
-
-SVM_2CLASS_PATTERN='s/Accuracy on test set: \([0-9]*.[0-9]*\)% ([0-9]* correct, [0-9]* incorrect, [0-9]* total)\.*/'
-SVM_NCLASS_PATTERN='s/Zero\/one-error on test set: \([0-9]*.[0-9]*\)% ([0-9]* correct, [0-9]* incorrect, [0-9]* total)\.*/'
-
 ###########################
 # Locking mechanism to avoid race conditions when parallel runs
-LOCKDIR="/tmp/yadmt.lock"
 function acquire_lock {
   LOCK_ACQUIRED="FALSE"
   while [ "$LOCK_ACQUIRED" == "FALSE" ]; 
@@ -103,19 +55,137 @@ function release_lock {
   rm -r "$LOCKDIR" &> /dev/null
 }
 ###########################
+CLEANED_UP="FALSE"
+function cleanup {
+  if [ "$CLEANED_UP" == "FALSE" ]; then
+    CLEANED_UP="TRUE"
+    # cleanup code: Don't delete lock on finish since it can be held by some other instance of this script
+    rm -rf $RANDOMIZED_INPUT_FILE $MODEL_FILE $TEMP_FILE1 $TEMP_FILE2 $TEMP_FILE3 $PRED_FILE $TRAIN_FILE $TEST_FILE &> /dev/null 
+    if [ "$EXPERIMENTAL_SETUP" != "1" ]; then 
+      FREE_DISK=`df /home | awk '{ print $5 }' | tail -n 1 | sed 's/%//'`
+      if [ "$FREE_DISK" -gt "$MAXIMUM_DISK_SPACE_TO_USE" ]; then
+        acquire_lock
+        # Least recently used algorithm
+        # Delete all but the most recent 2 input files
+        cd $YADMT_DIR"/data"
+        (ls -t|head -n 2;ls)|sort|uniq -u|xargs rm
+        release_lock
+      fi
+    fi  
+  fi
+}
+# Trap user interrupts
+trap "cleanup; exit 0" SIGHUP SIGINT SIGTERM
+trap "cleanup;" EXIT
+trap "cleanup;" ERR
+###########################
 
+#Read variables: MAX_MEMORY_CLASSIFIER, ACCURACY_FILE, RESULTS_FILE, MASTER, EXPERIMENTAL_SETUP
+source $CONFIG_FILE
+
+mkdir $YADMT_DIR"/data" &> /dev/null
+
+# Only input files go in data directory
+RANDOMIZED_INPUT_FILE=$YADMT_DIR"/input-"$RANDOM"-"$RANDOM"-"$RANDOM".txt"
+
+if [ "$EXPERIMENTAL_SETUP" == "2" ]; then
+  TEMP_INPUT_FILE=$INPUT_FILE
+  ONLY_FILE_NAME_WITH_EXT=$(basename "$INPUT_FILE")
+  INPUT_FILE=$YADMT_DIR"/data/"$ONLY_FILE_NAME_WITH_EXT
+
+  # Double checking for efficiency so that you don't have to acquire lock 
+  if [ ! -e $INPUT_FILE ]; then
+    acquire_lock
+    # So that only 1 process can download a file
+    # Also, since control file ensures that input files are listed sequentially, this is really efficient mechanism !!!
+    if [ ! -e $INPUT_FILE ]; then
+      # Get file if it doesnot exists
+      scp $MASTER":"$TEMP_INPUT_FILE $INPUT_FILE
+    fi
+    release_lock
+  fi
+elif [ "$EXPERIMENTAL_SETUP" == "3" ]; then
+  echo >&2 $PGM_NAME " - Amazon S3 service not supported yet."
+  exit 1
+fi
+
+acquire_lock
+# sort -R fails for Macbook Pro
+if ! sort -R $INPUT_FILE > $RANDOMIZED_INPUT_FILE 2> /dev/null
+then
+  echo >&2 $PGM_NAME " - Your system doesnot support \"sort -R\" option required to randomize the input file."
+  rm $RANDOMIZED_INPUT_FILE &> /dev/null
+  exit 1
+fi
+release_lock
 
 MODEL_FILE="model."$RANDOM"-"$RANDOM"-"$RANDOM".txt"
 TEMP_FILE1="tempFile."$RANDOM"-"$RANDOM"-"$RANDOM".txt"
 TEMP_FILE2="tempFile."$RANDOM"-"$RANDOM"-"$RANDOM".txt"
 TEMP_FILE3="tempFile."$RANDOM"-"$RANDOM"-"$RANDOM".txt"
 PRED_FILE="prediction."$RANDOM"-"$RANDOM"-"$RANDOM".txt"
+TRAIN_FILE=$RANDOMIZED_INPUT_FILE".train"
+TEST_FILE=$RANDOMIZED_INPUT_FILE".test"
+
+# cleanup code if user decides to abort the execution of this script
+VAL_ONE="1"
+
+# For weka, maximum size of memory in MB
+if [ -z "${MAX_MEMORY_CLASSIFIER}" ]; then
+  MAX_MEMORY_CLASSIFIER=1000
+fi
+
+###########################
+
+# Generate train and test files
+acquire_lock
+# To get NUM_LINES, use command given below, not `wc -l $INPUT_FILE`
+NUM_LINES=`cat $INPUT_FILE | wc -l`
+release_lock
+
+NUM_TRAIN=$(($NUM_LINES*80/100)) 
+NUM_TEST=$(($NUM_LINES*20/100))
+
+# Sanity check
+TOTAL_ITEMS=$((NUM_TRAIN+NUM_TEST))
+if [ "$TOTAL_ITEMS" -gt "$NUM_LINES" ]; then
+  echo >&2 $PGM_NAME " - Incorrect number for training and test data"
+  exit 1
+fi
+
+cat $RANDOMIZED_INPUT_FILE | head -n $NUM_TRAIN > $TRAIN_FILE
+cat $RANDOMIZED_INPUT_FILE | tail -n $NUM_TEST > $TEST_FILE
+
+###########################
+
+# Get parameters for classifer. Example: NUM_FEATURES, NUM_CLASSES and PARAM_NAME
+METAINFO_PGM=$YADMT_DIR"/"GetMetaInformationOfInputSVM
+if ! NUM_FEATURES=`$METAINFO_PGM $RANDOMIZED_INPUT_FILE numFeatures` 2> /dev/null
+then
+  echo >&2 $PGM_NAME " - Incorrect classes in input file:" $INPUT_FILE  "Make sure you assign values for all classes (eg: you cannot have classes 1,2,3,5). If there are just 2 classes, assign values -1 and 1."
+  rm $RANDOMIZED_INPUT_FILE $TRAIN_FILE $TEST_FILE &> /dev/null
+  exit 1
+fi
+if ! NUM_CLASSES=`$METAINFO_PGM $RANDOMIZED_INPUT_FILE numClasses` 2> /dev/null
+then
+  echo >&2 $PGM_NAME " - Incorrect classes in input file1:" $INPUT_FILE  "Make sure you assign values for all classes (eg: you cannot have classes 1,2,3,5). If there are just 2 classes, assign values -1 and 1."
+  rm $RANDOMIZED_INPUT_FILE $TRAIN_FILE $TEST_FILE &> /dev/null
+  exit 1
+fi
+
+PARAM_NAME=`$PROGRAMX $RANDOMIZED_INPUT_FILE`
+
+###########################
+
+SVM_2CLASS_PATTERN='s/Accuracy on test set: \([0-9]*.[0-9]*\)% ([0-9]* correct, [0-9]* incorrect, [0-9]* total)\.*/'
+SVM_NCLASS_PATTERN='s/Zero\/one-error on test set: \([0-9]*.[0-9]*\)% ([0-9]* correct, [0-9]* incorrect, [0-9]* total)\.*/'
+
 
 if [ "$NUM_CLASSES" == "2" ]; then
   # SVM Light script
   if [ "$CLASSIFIER" == "svm_linear" ]; then
-    ./svm_learn -t 0 $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
-    ./svm_classify -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_learn" -t 0 $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_classify" -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
     acquire_lock
     echo "" >> $RESULTS_FILE
     echo "svm_linear: Results for input file:" $INPUT_FILE "cycle:" $CYCLE_NUM "parameter:" $PARAM_NAME >> $RESULTS_FILE
@@ -125,8 +195,8 @@ if [ "$NUM_CLASSES" == "2" ]; then
     sed "$SVM_2CLASS_PATTERN$CYCLE_NUM svm_linear 0 "$PARAM_NAME" \1/g" $TEMP_FILE1 >> $ACCURACY_FILE
     release_lock
   elif [ "$CLASSIFIER" == "svm_poly" ]; then
-    ./svm_learn -t 1 -d $DEGREE $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
-    ./svm_classify -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_learn" -t 1 -d $DEGREE $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_classify" -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
     acquire_lock
     echo "" >> $RESULTS_FILE
     echo "svm_poly: Results for input file:" $INPUT_FILE "cycle:" $CYCLE_NUM "parameter:" $PARAM_NAME "DEGREE:" $DEGREE >> $RESULTS_FILE
@@ -136,8 +206,8 @@ if [ "$NUM_CLASSES" == "2" ]; then
     sed "$SVM_2CLASS_PATTERN$CYCLE_NUM svm_poly "$DEGREE" "$PARAM_NAME" \1/g" $TEMP_FILE1 >> $ACCURACY_FILE
     release_lock
   elif [ "$CLASSIFIER" == "svm_rbf" ]; then
-    ./svm_learn -t 2 -g $GAMMA $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
-    ./svm_classify -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_learn" -t 2 -g $GAMMA $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_classify" -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
     acquire_lock
     echo "" >> $RESULTS_FILE
     echo "svm_rbf: Results for input file:" $INPUT_FILE "cycle:" $CYCLE_NUM "parameter:" $PARAM_NAME "GAMMA:" $GAMMA >> $RESULTS_FILE
@@ -152,8 +222,8 @@ else
   # SVM Multiclass script
   TRADE_OFF=0.01
   if [ "$CLASSIFIER" == "svm_linear" ]; then
-    ./svm_multiclass_learn -c $TRADE_OFF -t 0 $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
-    ./svm_multiclass_classify -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_multiclass_learn" -c $TRADE_OFF -t 0 $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_multiclass_classify" -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
     acquire_lock
     echo "" >> $RESULTS_FILE
     echo "svm_linear: (multi-class) Results for input file:" $INPUT_FILE "cycle:" $CYCLE_NUM "parameter:" $PARAM_NAME >> $RESULTS_FILE
@@ -167,8 +237,8 @@ else
     sed "$SVM_NCLASS_PATTERN$CYCLE_NUM svm_linear 0 "$PARAM_NAME" \1/g" $TEMP_FILE1 >> $ACCURACY_FILE
     release_lock
   elif [ "$CLASSIFIER" == "svm_poly" ]; then
-    ./svm_multiclass_learn -c $TRADE_OFF -t 1 -d $DEGREE $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
-    ./svm_multiclass_classify -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_multiclass_learn" -c $TRADE_OFF -t 1 -d $DEGREE $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_multiclass_classify" -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
     acquire_lock
     echo "" >> $RESULTS_FILE
     echo "svm_poly: (multi-class) Results for input file:" $INPUT_FILE "cycle:" $CYCLE_NUM "parameter:" $PARAM_NAME "DEGREE:" $DEGREE >> $RESULTS_FILE
@@ -182,8 +252,8 @@ else
     sed "$SVM_NCLASS_PATTERN$CYCLE_NUM svm_poly "$DEGREE" "$PARAM_NAME" \1/g" $TEMP_FILE1 >> $ACCURACY_FILE
     release_lock
   elif [ "$CLASSIFIER" == "svm_rbf" ]; then
-    ./svm_multiclass_learn -c $TRADE_OFF -t 2 -g $GAMMA $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
-    ./svm_multiclass_classify -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_multiclass_learn" -c $TRADE_OFF -t 2 -g $GAMMA $TRAIN_FILE $MODEL_FILE > $TEMP_FILE1
+    $YADMT_DIR"/svm_multiclass_classify" -v 1 $TEST_FILE $MODEL_FILE $PRED_FILE > $TEMP_FILE1
     acquire_lock
     echo "" >> $RESULTS_FILE
     echo "svm_rbf: (multi-class) Results for input file:" $INPUT_FILE "cycle:" $CYCLE_NUM "parameter:" $PARAM_NAME "DEGREE:" $DEGREE >> $RESULTS_FILE
@@ -201,14 +271,14 @@ else
 fi
 
 if [ "$CLASSIFIER" == "naive_bayes" ]; then
-  if ! ./GenerateArffFiles $TRAIN_FILE $TEMP_FILE1".arff" > $TEMP_FILE3 2> $TEMP_FILE1
+  if ! $YADMT_DIR"/GenerateArffFiles" $TRAIN_FILE $TEMP_FILE1".arff" > $TEMP_FILE3 2> $TEMP_FILE1
   then
     DETAIL_ERROR=`cat $TEMP_FILE1` # Print for debugging
     echo >&2 $PGM_NAME " - Error while generating arff files."
     rm $TEMP_FILE1 $TEMP_FILE2 $TEMP_FILE3 $TEMP_FILE1".arff" $TEMP_FILE2".arff" &> /dev/null
     exit 1
   fi
-  if ! ./GenerateArffFiles $TEST_FILE $TEMP_FILE2".arff" >> $TEMP_FILE3 2> $TEMP_FILE1
+  if ! $YADMT_DIR"/GenerateArffFiles" $TEST_FILE $TEMP_FILE2".arff" >> $TEMP_FILE3 2> $TEMP_FILE1
   then
     DETAIL_ERROR=`cat $TEMP_FILE1` # Print for debugging
     echo >&2 $PGM_NAME " - Error while generating arff files."
@@ -217,7 +287,7 @@ if [ "$CLASSIFIER" == "naive_bayes" ]; then
   fi
   TEMP_FILE3_DATA=`cat $TEMP_FILE3`
   if [ "$TEMP_FILE3_DATA" == "" ]; then
-    if ! java "-Xmx"$MAX_MEMORY_CLASSIFIER"m" -cp ./weka.jar weka.classifiers.bayes.NaiveBayes -t $TEMP_FILE1".arff" -T $TEMP_FILE2".arff" > $TEMP_FILE3 2>>  $RESULTS_FILE
+    if ! java "-Xmx"$MAX_MEMORY_CLASSIFIER"m" -cp $YADMT_DIR"/weka.jar" weka.classifiers.bayes.NaiveBayes -t $TEMP_FILE1".arff" -T $TEMP_FILE2".arff" > $TEMP_FILE3 2>>  $RESULTS_FILE
     then
       echo >&2 $PGM_NAME " - Error while executing weka naive bayes (See " $RESULTS_FILE " for detailed description of error).\n"
       rm $TEMP_FILE1 $TEMP_FILE2 $TEMP_FILE3 $TEMP_FILE1".arff" $TEMP_FILE2".arff" &> /dev/null
@@ -241,6 +311,4 @@ if [ "$CLASSIFIER" == "naive_bayes" ]; then
   fi
   
 fi
-
-rm $RANDOMIZED_INPUT_FILE $TRAIN_FILE $TEST_FILE $TEMP_FILE1 $TEMP_FILE2 $MODEL_FILE $PRED_FILE &> /dev/null
 
--- src/yadmt.sh
+++ src/yadmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  initialize.sh configFile [TODO For Multiple machines: loginFile]
+#  yadmt configFile [TODO For Multiple machines: loginFile]
 #
 #  To use loginFile, you need to have password-less ssh
 #  ssh-copy-id -i ~/.ssh/id_rsa.pub username@remote-server
@@ -9,54 +9,324 @@
 
 # Has to be of format "configFile,inputFile,cycleNum,classifier,optional_params" without any spaces
 PGM_NAME=$0
-CONFIG_FILE=$1
+IS_WIZARD="FALSE"
+USER_HOME=$(eval echo ~${SUDO_USER})
+YADMT_DIR=$USER_HOME"/yadmt"
+USER_NAME=$(id -un)
+HOST_NAME=$(hostname --long)
+
+LOGIN_FILE=$YADMT_DIR"/login.txt"
+
+# Delete lock file before starting yadmt
+rm -r /tmp/yadmt.lock/ &> /dev/null
+
+###########################
+# cleanup code
+CLEANED_UP="FALSE"
+function cleanup {
+  if [ "$CLEANED_UP" == "FALSE" ]; then
+    CLEANED_UP="TRUE"
+    rm -rf $CONFIG_FILE $CONTROL_FILE &> /dev/null
+  fi
+}
+# Trap user interrupts
+trap "cleanup; exit 0" SIGHUP SIGINT SIGTERM
+trap "cleanup;" EXIT
+trap "cleanup;" ERR
+###########################
+
+
+if [ "$#" == "0" ]; then
+  echo >&2 $PGM_NAME " - Expected an argument. Try running" $PGM_NAME "--wizard"
+  exit 1
+elif [ "$1" == "--wizard" ]; then
+##############################
+# Wizard
+IS_WIZARD="TRUE"
+
+# 1 - Single machine, 2 - multiple machines (accessible via ssh), 3 - EC2 cluster 
+echo "yadmt supports following configuration:"
+echo "1. Single machine"
+echo "2. Multiple machines (accessible via passwordless ssh)"
+echo -n "Which configuration do you want to run (1,2):"
+read ANS
+echo ""
+
+if [ "$ANS" == "1" ]; then 
+  EXPERIMENTAL_SETUP="1"
+  echo "OK, will run yadmt on this machine only."
+  echo ""
+elif [ "$ANS" == "2" ]; then 
+
+  echo -n "Do you want to use this machine i.e. " $USER_NAME"@"$HOST_NAME "(y/n):"
+  read ANS
+  echo ""
+  if [ "$ANS" == "y" ]; then
+    echo ":" > $LOGIN_FILE
+  elif [ "$ANS" == "n" ]; then
+    echo -n "" > $LOGIN_FILE
+  else
+    echo >&2 $PGM_NAME " - Invalid input."
+    exit 1
+  fi
+
+  ANS="y"
+  while "$ANS" == "y"
+  do
+    echo -n "Do you want to use more machine (y/n):"
+    read ANS
+    if [ "$ANS" == "y" ]; then
+      echo "Login info of a remote machine can be username@remote-machine or just remote-machine"
+      echo -n "Enter login info:"
+      read REMOTE_LOGIN_INFO
+      echo $REMOTE_LOGIN_INFO >> $LOGIN_FILE
+    fi
+  done
+  
+  echo ""
+
+  echo "For sharing data across these machines, yadmt allows following option:"
+  echo "1. SCP file transfer among machines"
+  echo "2. Amazon S3"
+  echo -n "Which option do you want(1,2):"
+  read ANS
+  echo ""
+  if [ "$ANS" == "1" ]; then 
+    EXPERIMENTAL_SETUP="2"
+  elif [ "$ANS" == "2" ]; then 
+    EXPERIMENTAL_SETUP="3"
+  else
+    echo >&2 $PGM_NAME " - Invalid option."
+    exit 1
+  fi
+else
+  echo >&2 $PGM_NAME " - Invalid configuration."
+  exit 1
+fi
+
+
+echo "yadmt supports following tasks:"
+echo "1. Classification"
+echo "2. Topic Modelling"
+echo -n "Which task do you wish to perform (1,2,...):"
+read TASK1
+echo ""
+
+CONFIG_FILE=$YADMT_DIR"/config.txt"
+CONTROL_FILE=$YADMT_DIR"/controlFile.txt"
+
+
+echo "EXPERIMENTAL_SETUP="$EXPERIMENTAL_SETUP > $CONFIG_FILE
+echo "MASTER="$USER_NAME"@"$HOST_NAME >> $CONFIG_FILE
+# This means when disk space on home reaches 95%, don't keep any more file in disk cache (i.e. ~/yadmt/data)
+echo "MAXIMUM_DISK_SPACE_TO_USE=95" >> $CONFIG_FILE
+
+if [ "$TASK1" == "1" ]; then
+  # Classification
+
+  TASK="CLASSIFICATION"
+  echo "TASK="$TASK >> $CONFIG_FILE  
+
+  echo "yadmt supports following classifiers:"
+  echo "svm_linear svm_poly svm_rbf naive_bayes"
+  echo "(Hint: An example for below input is: svm_linear naive_bayes)" 
+  echo -n "Enter the list of classifiers separated by space you wish to run:"
+  read CLASSIFIERS
+  echo ""
+  echo "CLASSIFIERS=\""$CLASSIFIERS"\"" >> $CONFIG_FILE
+
+  
+  for classifier in $CLASSIFIERS
+  do
+    if [ "$classifier" == "naive_bayes" ]; then
+      echo "Classifiers such as Weka may require large amount of memory depending on the input size."
+      echo -n "Specify the amount of maximum memory in MB that a classifier can use (eg: 300, 1000):"
+      read MAX_MEMORY_CLASSIFIER
+      echo ""
+      echo "MAX_MEMORY_CLASSIFIER="$MAX_MEMORY_CLASSIFIER >> $CONFIG_FILE
+      
+    elif [ "$classifier" == "svm_linear" ]; then 
+      # Do nothing
+      echo -n ""
+    elif [ "$classifier" == "svm_poly" ]; then 
+      echo -n "Enter the list of degree separated by space you wish to run for svm_poly (eg: 2 3 4):"
+      read DEGREES_SVM_POLY
+      echo ""
+      echo "DEGREES_SVM_POLY=\""$DEGREES_SVM_POLY"\"" >> $CONFIG_FILE
+    elif [ "$classifier" == "svm_rbf" ]; then 
+      echo -n "Enter the list of gamma separated by space you wish to run for svm_rbf (eg: 0.001 0.01 0.1):"
+      read GAMMAS_SVM_RBF
+      echo ""
+      echo "GAMMAS_SVM_RBF=\""$GAMMAS_SVM_RBF"\"" >> $CONFIG_FILE
+    else
+      echo >&2 $PGM_NAME " - Incorrect classifier:" classifier
+      exit 1
+    fi
+  done
+  
+  echo -n "Enter the number of cycles you wish to run:"
+  read NUM_CYCLES
+  echo ""
+  echo "NUM_CYCLES="$NUM_CYCLES >> $CONFIG_FILE
+
+  echo -n "Enter the file name where you wish to store the accuracy (accuracy.txt):"
+  read ACCURACY_FILE
+  echo ""
+  echo "ACCURACY_FILE="$ACCURACY_FILE >> $CONFIG_FILE
+  
+  echo -n "Enter the file name where you wish to store the results by each classifier (result.txt):"
+  read RESULTS_FILE
+  echo ""
+  echo "RESULTS_FILE="$RESULTS_FILE >> $CONFIG_FILE
+
+  echo "Make sure that you specify absolute path for below files."
+  echo -n "Enter the file name that contains list of input files (files.txt):"
+  read FILES
+  echo ""
+  echo "FILES="$FILES >> $CONFIG_FILE
+
+  echo "Most experiments require to compare different models/parameters (example: features for CTM or LDA). yadmt assumes that:"
+  echo "- One input file (in format of SVMLight/SVMMulticlass) is generated per parameter."
+  echo "- For comparing different parameters, user has program X that takes the input file name and outputs string denoting the parameter name for that input file."
+  echo "- Default program X outputs the string \"parameter1\" for every input file"
+  echo -n "Do you want to use default program X (y/n):"
+  read ANS
+  echo ""
+  if [ "$ANS" == "n" ]; then
+    echo -n "Enter the path of user-defined program X:"
+    read PROGRAMX
+    echo ""
+  else 
+    PROGRAMX=$YADMT_DIR"/getParameterName"
+  fi
+  echo "PROGRAMX="$PROGRAMX >> $CONFIG_FILE
+  
+elif [ "$TASK1" == "2" ]; then
+  # Topic Modeling
+  TASK="TOPIC_MODELING"
+  echo "Topic modeling not supported yet"
+  exit 1
+fi
+
+
+##############################
+else
+  CONFIG_FILE=$1
+fi
+
+
 #LOGIN_FILE=$2
 
 # Has MAX_MEMORY_CLASSIFIER, ACCURACY_FILE, RESULTS_FILE + FILES, NUM_CYCLES, CLASSIFIERS, DEGREES_SVM_POLY, GAMMAS_SVM_RBF
-source "./"$CONFIG_FILE
+source $CONFIG_FILE
 
-CONTROL_FILE="controlFile-"$RANDOM"-"$RANDOM"-"$RANDOM".txt"
-rm $CONTROL_FILE &> /dev/null
+function cool_progress_ind {
+  chars=( "[ - ]" "[ \\ ]" "[ | ]" "[ / ]" )
+  interval=1
+  count=0
 
-for file in `cat $FILES`
-do
-  for cycle in `seq 1 $NUM_CYCLES`
+  echo -n "     "
+  while true
   do
-    for classifier in $CLASSIFIERS
+  pos=$(($count % 4))
+
+  echo -en "\b\b\b\b\b${chars[$pos]}"
+
+  count=$(($count + 1))
+  sleep $interval
+  done
+}
+
+function stop_progress_ind {
+  exec 2>/dev/null
+  kill $1
+  echo -en "\n"
+}
+
+
+if [ "$TASK" == "CLASSIFICATION" ]; then
+#----------------------------------------------
+  # Classification
+
+  for file in `cat $FILES`
+  do
+    for cycle in `seq 1 $NUM_CYCLES`
     do
-      if [ "$classifier" == "svm_poly" ]; then
-        for degree in $DEGREES_SVM_POLY
-        do
-          OUTPUT_STR=$CONFIG_FILE","$file","$cycle","$classifier","$degree
+      for classifier in $CLASSIFIERS
+      do
+        if [ "$classifier" == "svm_poly" ]; then
+          for degree in $DEGREES_SVM_POLY
+          do
+            OUTPUT_STR=$CONFIG_FILE","$file","$cycle","$classifier","$degree
+            echo $OUTPUT_STR >> $CONTROL_FILE
+          done
+        elif [ "$classifier" == "svm_rbf" ]; then
+          for gamma in $GAMMAS_SVM_RBF
+          do
+            OUTPUT_STR=$CONFIG_FILE","$file","$cycle","$classifier","$gamma
+            echo $OUTPUT_STR >> $CONTROL_FILE
+          done
+        else
+          OUTPUT_STR=$CONFIG_FILE","$file","$cycle","$classifier
           echo $OUTPUT_STR >> $CONTROL_FILE
-        done
-      elif [ "$classifier" == "svm_rbf" ]; then
-        for gamma in $GAMMAS_SVM_RBF
-        do
-          OUTPUT_STR=$CONFIG_FILE","$file","$cycle","$classifier","$gamma
-          echo $OUTPUT_STR >> $CONTROL_FILE
-        done
-      else
-        OUTPUT_STR=$CONFIG_FILE","$file","$cycle","$classifier
-        echo $OUTPUT_STR >> $CONTROL_FILE
-      fi
+        fi
+      done
     done
   done
-done
 
-# Randomizing the control file helps in load balancing as different classifiers have different requirements (cpu-demanding, memory demanding, etc)
-sort -R $CONTROL_FILE > $CONTROL_FILE".temp"
+  # Replacing xargs by more powerful gnu-parallel: cat $CONTROL_FILE | xargs --max-args=1 --max-procs=100 ./runClassifier.sh &
+  # Delayed start makes sure that machines don't get overloaded. It may however adversely affect small jobs. However, the assumption is that running a classification task is usually extremely time-consuming task, so sleeping for few second should not affect overall performance drasctically.
+  # TODO For Multiple machines: --sshloginfile $LOGIN_FILE
+  RUN_CLASSIFIER_PATH=$YADMT_DIR"/runClassifier"
+  if [ "$IS_WIZARD" == "TRUE" ]; then
+    echo -n "Running classification task. Please wait "
+    cool_progress_ind &
+    pid=$!
+    # If yadmt is interrupted, this will take care of killing progress bar
+    trap "stop_progress_ind $pid; exit" INT TERM EXIT
 
-# Replacing xargs by more powerful gnu-parallel: cat $CONTROL_FILE | xargs --max-args=1 --max-procs=100 ./runClassifier.sh &
-# Delayed start makes sure that machines don't get overloaded. It may however adversely affect small jobs.
-# However, the assumption is that running a classification task is usually extremely time-consuming task, so sleeping for few second
-# should not affect overall performance drasctically.
-# TODO For Multiple machines: --sshloginfile $LOGIN_FILE
-cat $CONTROL_FILE".temp" | parallel --max-args=1 --load 95% 'sleep $((RANDOM*4/32767+1)); ~/yadmt/runClassifier {}'
+    if [ "$EXPERIMENTAL_SETUP" == "1"  ]; then
+      cat $CONTROL_FILE | parallel --max-args=1 --load 95% "sleep $((RANDOM*2/32767+1));"$RUN_CLASSIFIER_PATH' {};'
+    else
+      # first delete previous data folder
+      parallel --sshloginfile $LOGIN_FILE "rm -rf "$YADMT_DIR"/data" &> /dev/null
+      # then start the program
+      cat $CONTROL_FILE | parallel --max-args=1 --sshloginfile $LOGIN_FILE --load 95% "sleep $((RANDOM*2/32767+1));"$RUN_CLASSIFIER_PATH' {};'
+      # delete after finishing the program
+      parallel --sshloginfile $LOGIN_FILE "rm -rf "$YADMT_DIR"/data" &> /dev/null
+    fi
+
+    stop_progress_ind $pid
+  else
+      if [ "$EXPERIMENTAL_SETUP" == "1"  ]; then
+        cat $CONTROL_FILE | parallel --max-args=1 --load 95% "sleep $((RANDOM*2/32767+1));"$RUN_CLASSIFIER_PATH' {}'
+      else
+        # first delete previous data folder
+        parallel --sshloginfile $LOGIN_FILE "rm -rf "$YADMT_DIR"/data" &> /dev/null
+        # then start the program
+        cat $CONTROL_FILE | parallel --max-args=1 --sshloginfile $LOGIN_FILE --load 95% "sleep $((RANDOM*2/32767+1));"$RUN_CLASSIFIER_PATH' {}'
+        # delete after finishing the program
+        parallel --sshloginfile $LOGIN_FILE "rm -rf "$YADMT_DIR"/data" &> /dev/null
+      fi
+  fi
+
+  if [ "$IS_WIZARD" == "TRUE" ]; then
+    echo "Done with classification task."
+  fi
+
+  # Use --tag to debug
+  # TODO For Multiple machines:
+  # parallel --nonall --sshloginfile $LOGIN_FILE 'cat $ACCURACY_FILE' > $ACCURACY_FILE".final" 
+
+# Classification ends
+#----------------------------------------------
 
 
-rm $CONTROL_FILE".temp" $CONTROL_FILE &> /dev/null
+elif [ "$TASK" == "TOPIC_MODELING" ]; then
+#----------------------------------------------
+  # Topic Modeling
+  echo -n "" 
 
-# Use --tag to debug
-# TODO For Multiple machines:
-# parallel --nonall --sshloginfile $LOGIN_FILE 'cat $ACCURACY_FILE' > $ACCURACY_FILE".final" 
+  # Topic Modeling ends
+#----------------------------------------------
+fi
